cc5854cbc24df6f33ae19dcc2a8ed3bd844080a2
eleandrovanelli <eleandro.vanelli@ciss.com.br>
2025-08-19 20:54:19 -0300

    logos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
     path: 'login',
     loadChildren: () => import('./pages/login/login.routes').then(rota => rota.routes)
   }
