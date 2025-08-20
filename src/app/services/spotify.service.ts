3f2b1032d7fa7250c55e09e6a3f523a9dfa0dfc4
eleandrovanelli <eleandro.vanelli@ciss.com.br>
2025-08-19 22:31:30 -0300

    Ajuste

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -9,7 +9,23 @@ export class SpotifyService {
   constructor() {}
 
   async obterUrlLogin(): Promise<string> {
-
+    const codigoAleatorio = await this.gerarCodigoAleatorio();
     const authPoint = `${SpotifyConfiguration.authEndpoint}?`;
+    const clientId = `client_id=${SpotifyConfiguration.clientId}&`;
+    const urlRedirect = `redirect_uri=${SpotifyConfiguration.redirectUrl}&`;
+    const scopes = `scope=${SpotifyConfiguration.scopes.join('%20')}`;
+    const codeChallengeMethod = 'code_challenge_method=S256';
+    const codeChallengeParam = 'code_challenge' + codigoAleatorio + '&';
+    const responseType = 'response_type=code';
+
+    return `${authPoint}${clientId}${urlRedirect}${scopes}
+      ${codeChallengeMethod}${codeChallengeParam}${responseType}`;
+  }
+
+  async gerarCodigoAleatorio() {
+    // const codigoVerificador = this.gerarCodigoVerificador(128);
+    // const codChallenge = await this.gerarHash(codigoVerificador);
+
+    return ''; //codChallenge;
   }
 }
