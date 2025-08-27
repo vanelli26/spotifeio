ab22b7283e1c31c44d29c26930323767166bf1b0
eleandrovanelli <eleandro.vanelli@ciss.com.br>
2025-08-26 21:46:56 -0300

    Codigo Verificador

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
 export const routes: Routes = [
   {
@@ -9,5 +9,9 @@ export const routes: Routes = [
   {
     path: 'login',
     loadChildren: () => import('./pages/login/login.routes').then(rota => rota.routes)
+  },
+  {
+    path: 'player',
+    loadChildren: () => import('./pages/player/player.routes').then(rota => rota.routes)
   }
 ];
